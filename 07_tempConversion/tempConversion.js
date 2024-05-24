82251368b670fd2122fba4fed34528526adb42fb
Charu <charumishra5673@gmail.com>
2024-05-24 12:17:15 +0530

    7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  f-=32;
+  f*=5;
+  f/=9;
+  return Math.round(f*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+c*=9;
+c/=5;
+c+=32;
+return Math.round(c*10)/10;
 };
 
 // Do not edit below this line
