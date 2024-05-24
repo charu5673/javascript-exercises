e6d2966f8ac1770c39309cb43723fee2b45060f0
Charu <charumishra5673@gmail.com>
2024-05-24 12:01:35 +0530

    3,4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
-
+let a=arguments[0];
+let n=arguments.length;
+let m=a.length;
+let i,j,flag=false;;
+let b=[];
+for(i=0;i<m;i++)
+    {
+        flag=false;
+        for(j=1;j<n;j++)
+            {
+                if(a[i]===arguments[j])
+                    flag=true;
+            }
+            if(!flag)
+                b.push(a[i]);
+    }
+    return b;
 };
 
 // Do not edit below this line
