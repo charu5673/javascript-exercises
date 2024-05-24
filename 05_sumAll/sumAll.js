5a2f6bd5a59b34ac3a6ba39eb3a5a5933674d25c
Charu <charumishra5673@gmail.com>
2024-05-24 12:09:10 +0530

    5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+if(typeof(a)!=='number'||typeof(b)!=='number'||a<0||b<0)
+    return("ERROR");
+if(a>b)
+{
+    b=a+b;
+    a=b-a;
+    b=b-a;
+}
+    return(((b-a+1)/2)*(2*a+(b-a)));
 };
 
 // Do not edit below this line
